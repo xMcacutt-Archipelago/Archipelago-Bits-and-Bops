2917cd45147f912a9a334d2f745206d04b6d0ac5
Dashieswag92 <rainbowdashie222@gmail.com>
2026-01-05 12:27:44 -0600

    change base info to bits_and_bops

---
+++ worlds/bits_and_bops/__init__.py
@@ -0,0 +1,86 @@
+import typing
+from BaseClasses import Item, MultiWorld, Tutorial, ItemClassification, Region, Location
+from worlds.AutoWorld import WebWorld, World
+from .items import BitsAndBopsItem, bits_and_bops_item_table, create_items, ItemData, place_locked_items
+from .locations import bits_and_bops_location_table, BitsAndBopsLocation
+from .options import BitsAndBopsOptions, bits_and_bops_option_groups
+from .regions import create_regions, connect_regions, connect_all_regions
+from .rules import set_rules
+
+class BitsAndBopsWeb(WebWorld):
+    theme = "grass"
+
+    setup_en = Tutorial(
+        "Multiworld Setup Guide",
+        "A guide to setting up the Bits & Bops randomizer connected to an Archipelago Multiworld.",
+        "English",
+        "setup_en.md",
+        "setup/en",
+        ["xMcacutt", "Dashieswag92"]
+    )
+
+    tutorials = [setup_en]
+    option_groups = bits_and_bops_option_groups
+
+class BitsAndBopsWorld(World):
+    """
+    Bits & Bops is a collection of original rhythm mini-games!
+     Filled with catchy music, snappy gameplay and gorgeous, hand-drawn animation,
+      Bits & Bops is sure to brighten your day.
+    """
+    game = "Bits & Bops"
+    options_dataclass = BitsAndBopsOptions
+    options: BitsAndBopsOptions
+    topology_present = True
+    item_name_to_id = {name: item.code for name, item in bits_and_bops_item_table.items()}
+    location_name_to_id = {name: item.code for name, item in bits_and_bops_location_table.items()}
+
+    web = BitsAndBopsWeb()
+
+    def __init__(self, multiworld: MultiWorld, player: int):
+        super().__init__(multiworld, player)
+        self.itempool = []
+
+    def fill_slot_data(self) -> id:
+        return {
+            "ModVersion": "1.0.0",
+            "Goal": self.options.goal.value,
+            "DeathLink": self.options.death_link.value,
+        }
+
+    def generate_early(self) -> None:
+        return
+
+    def create_item(self, name: str) -> Item:
+        item_info = bits_and_bops_item_table[name]
+        return BitsAndBopsItem(name, item_info.classification, item_info.code, self.player)
+
+    def create_items(self):
+        create_items(self)
+
+    def create_event(self, region_name: str, event_name: str) -> None:
+        region: Region = self.multiworld.get_region(region_name, self.player)
+        loc: BitsAndBopsLocation = BitsAndBopsLocation(self.player, event_name, None, region)
+        loc.place_locked_item(BitsAndBopsItem(event_name, ItemClassification.progression, None, self.player))
+        region.locations.append(loc)
+
+    def create_regions(self):
+        create_regions(self)
+        place_locked_items(self)
+        connect_all_regions(self)
+
+    def set_rules(self):
+        set_rules(self)
+
+    def extend_hint_information(self, hint_data: typing.Dict[int, typing.Dict[int, str]]):
+        new_hint_data = {}
+
+        for key, data in bits_and_bops_location_table.items():
+            try:
+                location: Location = self.multiworld.get_location(key, self.player)
+            except KeyError:
+                continue
+
+            # new_hint_data[location.address] = f""
+
+        hint_data[self.player] = new_hint_data
